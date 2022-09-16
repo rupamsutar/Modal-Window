c59493d83b006fa8290ca63ae404d4a1505546ca
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-16 16:07:55 +0530

    Working with classes in DOM

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ const btnsShowModal = document.querySelectorAll(".show-modal");
 console.log(btnsShowModal);
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-    console.log(btnsShowModal[i].textContent);
-}
+    btnsShowModal[i].addEventListener("click", openModal);    
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+function closeModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+function openModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
