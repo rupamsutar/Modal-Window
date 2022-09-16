ccfb970d5a088edacac6753364044235843d8d8d
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-16 16:35:19 +0530

    escape keypress added

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+})
+
 function closeModal() {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
